68d839433092d357570ae046dd5a3edf889d49fd
Shawn Humphries <smh266@humboldt.edu>
2021-05-11 12:58:40 -0700

    added embed !help message

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ client = discord.Client()
 async def on_ready():
     print(f'{client.user.name} has connected to Discord!')
 
+# sends a message to new user in discord channel
 @client.event
 async def on_member_join(member):
     
@@ -20,9 +21,11 @@ async def on_member_join(member):
         if str(channel) == "general":
             await client.send_message(f'welcome to the server' + {member.mention})
 
+#Let's the server know SRB is ready to use
 @client.event
-async def on_member__remove(member):
-    print(f'{member} has departed the server.')
+async def on_ready():
+    general_channel = client.get_channel(805505983161827372)
+    await general_channel.send('SRB has arrived!')
 
 
 # variables representing responses 
@@ -49,24 +52,46 @@ replies = [
     'Good luck',
     'Anytime',
     'No worries'
-
 ]
 
-# if message from user starts with 'Hey Apology bot' the bot replies 'Do you need an apology?'
+maybe = [
+    'Make up your mind',
+    'I do not have all day, hurry up and decide',
+    'Go ask someone else then',
+    'Stop waisting my time',
+    'Venmo me $5 and I will give you an apology'
+]
+
+# random on_message events
 
 @client.event
 async def on_message(message):
-    if message.content.startswith('Hey Apology bot'):
+
+    if message.content.startswith('!help'):
+         general_channel = client.get_channel(805505983161827372)
+         myEmbed = discord.Embed(title='SRB Current Version', description='SRB Version 1.1', color=0xFF0000)
+         myEmbed.add_field(name='Version:', value='1.1', inline=False)
+         myEmbed.add_field(name='Date Premiered', value='May 12, 2021', inline=False)
+         myEmbed.set_author(name='Shawn Humphries')
+         myEmbed.set_footer(text='This bot does a lot of random things, I have no idea what I am doing...but I am having fun')
+
+         await general_channel.send(embed=myEmbed)
+
+    if message.content.startswith('Hey SRB'):
         await message.channel.send('Do you need an apology?')
-    
     if message.content.startswith('!yes'):
         await message.channel.send(random.choice(apologies))
-    elif  message.content.startswith('!no'):
+        time.sleep(3)
+        await message.channel.send('Would you like another apology?')
+    if  message.content.startswith('!no'):
         await message.channel.send(random.choice(denials))
-    
     if message.content.startswith('!thanks'):
         await message.channel.send(random.choice(replies))
+    if message.content.startswith('!maybe'):
+        await message.channel.send(random.choice(maybe))
     
+    # message to play "what are the odds"
+
     if message.content.startswith('what are the odds?'):
         await message.channel.send("Congratulations! You have started 'What Are the Odds! Follow the next steps. There is No going back now!")
         time.sleep(4)
